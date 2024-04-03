182034c36580e356ed4a9bf76c91b1c879dcdacd
MT <mitutumi0210@gmail.com>
2024-04-03 19:40:34 +0900

    Update

---
+++ backend/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.18.1',  # 例としてnumpyパッケージを依存関係として追加
+        'pandas>=1.0.1',
+        'flask',
+        'flask_cors'
+    ]
+)
